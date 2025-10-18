bde14d4e5e8733d91bbdfc619e8ca822f2aa3b4c
hellvita <seredaolga03@gmail.com>
2025-10-18 14:23:58 +0200

    use async/await, add the load more button

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -2,15 +2,16 @@ import axios from 'axios';
 
 const API_KEY = '52683370-9ae3cf283454fd0e20897a7f1';
 const BASE_URL = 'https://pixabay.com/api/';
+export const PER_PAGE = 9;
 
 const API_PARAMS = {
   image_type: 'photo',
   orientation: 'horizontal',
   safesearch: true,
-  per_page: 36,
+  per_page: PER_PAGE,
 };
 
-export function getImagesByQuery(query) {
+export function getImagesByQuery(query, page) {
   if (!query || typeof query !== 'string') {
     return Promise.reject(new Error('Invalid query parameter'));
   }
@@ -18,14 +19,9 @@ export function getImagesByQuery(query) {
   const params = {
     key: API_KEY,
     q: query,
+    page,
     ...API_PARAMS,
   };
 
-  return axios
-    .get(BASE_URL, { params })
-    .then(response => response.data.hits)
-    .catch(error => {
-      console.error('API Error:', error.message);
-      throw error;
-    });
+  return axios.get(BASE_URL, { params });
 }
--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
-import { getImagesByQuery } from './js/pixabay-api';
+import { getImagesByQuery, PER_PAGE } from './js/pixabay-api';
 import * as render from './js/render-functions';
 import { toastMessage } from './js/settings';
 
 const formEl = document.querySelector('.form');
 const inputEl = formEl.elements['search-text'];
+const loadBtnEl = document.querySelector('.load-btn');
+
+let currentPage = 1;
+let pagesAmount = 0;
+let query = '';
 
 formEl.addEventListener('submit', onSearch);
 
@@ -13,33 +18,82 @@ function onSearch(e) {
   formEl.reset();
 }
 
-function handleImageSearch() {
-  const query = inputEl.value.trim();
+async function handleImageSearch() {
+  try {
+    query = inputEl.value.trim();
 
-  if (!query) {
-    toastMessage('&#128269; enter the query to search');
-    return;
+    if (!query) {
+      toastMessage('&#128269; enter the query to search');
+      return;
+    }
+
+    render.clearGallery();
+    render.showLoader();
+
+    currentPage = 1;
+    hideLoadBtn();
+
+    const { data } = await getImagesByQuery(query, currentPage);
+    const images = data.hits;
+
+    render.hideLoader();
+
+    if (images.length === 0) {
+      toastMessage(
+        'Sorry, there are no images matching your search query. Please try again!'
+      );
+      return;
+    }
+
+    render.createGallery(images);
+
+    pagesAmount = Math.ceil(data.totalHits / PER_PAGE);
+    if (pagesAmount > 1) showLoadBtn();
+  } catch (error) {
+    handleError(error);
   }
+}
 
-  render.clearGallery();
-  render.showLoader();
+async function onLoadMore() {
+  try {
+    currentPage++;
+    hideLoadBtn();
+    render.showLoader();
 
-  getImagesByQuery(query)
-    .then(images => {
-      render.hideLoader();
+    const { data } = await getImagesByQuery(query, currentPage);
+    const images = data.hits;
 
-      if (images.length === 0) {
-        toastMessage(
-          'Sorry, there are no images matching your search query. Please try again!'
-        );
-        return;
-      }
+    render.hideLoader();
 
-      render.createGallery(images);
-    })
-    .catch(error => {
-      render.hideLoader();
-      toastMessage('Failed to fetch images. Please try again later.');
-      console.log(error);
-    });
+    if (images.length === 0) {
+      toastMessage(
+        'Sorry, there are no images matching your search query. Please try again!'
+      );
+      return;
+    }
+
+    render.createGallery(images);
+    if (pagesAmount > currentPage) showLoadBtn();
+    else
+      toastMessage(
+        "We're sorry, but you've reached the end of search results."
+      );
+  } catch (error) {
+    handleError(error);
+  }
+}
+
+function showLoadBtn() {
+  loadBtnEl.addEventListener('click', onLoadMore);
+  loadBtnEl.classList.remove('hidden');
+}
+function hideLoadBtn() {
+  loadBtnEl.removeEventListener('click', onLoadMore);
+  loadBtnEl.classList.add('hidden');
+}
+
+function handleError(error) {
+  render.hideLoader();
+  toastMessage('Failed to fetch images. Please try again later.');
+  console.log(error);
 }
